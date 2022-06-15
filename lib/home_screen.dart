d883c3c44e8fc738aaa8b190eee219f247b354af
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-06-15 07:51:45 +0530

    Transition Animation was set

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -25,13 +25,28 @@ class _HomeScreenState extends State<HomeScreen> {
                 onPressed: () {
                   Navigator.of(context).push(
                     PageRouteBuilder(
-                      // Base Navigation Builder and replacement of
-                      // MaterialPageRoute
-                      pageBuilder: ((context, animation, secondaryAnimation) {
-                        // Return the target Screen
-                        return const SecondScreen();
-                      }),
-                    ),
+                        // Base Navigation Builder and replacement of
+                        // MaterialPageRoute
+                        pageBuilder: ((context, animation, secondaryAnimation) {
+                          // Return the target Screen
+                          return const SecondScreen();
+                        }),
+
+                        // Duration for Navigation push
+                        transitionDuration: const Duration(seconds: 1),
+
+                        // Duration for Navigation pop
+                        reverseTransitionDuration: const Duration(seconds: 1),
+
+                        // Transition Animation
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          return ScaleTransition(
+                            scale: animation,
+                            alignment: Alignment.center,
+                            child: child,
+                          );
+                        }),
                   );
                 },
                 child: const Text('Navigate')),
