759dfa19b52f15c7ef00fd27d9e8ea160d196f2b
jinoshprabhuraj5 <jinoshprabhuraj5@gmail.com>
2022-06-15 07:12:42 +0530

    SecondScreen created

---
+++ lib/second_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: const Center(
+        child: Text(
+          'Welcome to\n Second Screen',
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
